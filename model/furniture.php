60c1385fa35b91ac7f49c8786c2700bae1486838
kennethmensah <kenneth.mensah@ashesi.edu.gh>
2016-03-24 19:48:49 +0000

    added data

--- model/furniture.php
+++ model/furniture.php
@@ -32,11 +32,11 @@ class furniture extends adb_object{
      * @param $image
      * @return bool|mysqli_stmt
      */
-    function addFurniture($type, $name, $description, $category, $image){
+    function addFurniture($type, $name, $description, $category, $brand,$image){
 
         //sql query
-        $str_query = "INSERT INTO furniture(furniture_type, name, description, category, image)
-                      VALUES (?, ?, ?, ?,?)";
+        $str_query = "INSERT INTO furniture(furniture_type, name, description, brand_id, category, image)
+                      VALUES (?, ?, ?, ?,?, ?)";
 
         $stmt = $this->prepareQuery($str_query);
 
@@ -44,7 +44,7 @@ class furniture extends adb_object{
             return false;
         }
 
-        $stmt->bind_param("issis", $type, $name, $description, $category, $image);
+        $stmt->bind_param("issiis", $type, $name, $description, $brand,$category, $image);
 
 
         $stmt->execute();
@@ -525,10 +525,189 @@ class furniture extends adb_object{
         return $stmt->get_result();
     }
 
+}
 
 
+$product_info = array(
+                array("name"=>"Edington", "brand"=>rand(1,15),
+                    "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+                    "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+                array("name"=>"American Attitude", "brand"=>rand(1,15),
+                    "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+                    "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Bancroft King", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Trevisco", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Kira", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Wyatt Media", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Cavallino", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Millenium Greensburg", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Konya", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Quinden", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Farrah", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Harlington", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Centennial LIV36", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Juararo", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Chilli Pepper Power", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic pepper pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Li Leather Longhorn Blackberry", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in Blackberry, brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Cooper Desert", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Murphy", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"St. Louis", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Porter", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Hawthorne Cooper", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Alder Creek", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Cooper Desert", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Mesh Porter", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Onyx", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Bayfield", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Walnut Heights", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Heights Cocktail", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Kianna", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Rhiannon Linen", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Jolene Leather", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Whitney", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Ottoman", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Becky II Ottoman", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Lucy Sette", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Faye Ottoman", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Shea Zed", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Mazzy Leather", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Daveigh", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Westwood Lisa", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Lisa Three-piece", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Geena Terri", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Tatiana Ottoman", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Zuma Linen", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Classic Lisa", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Quin Snow", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Celeste", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Kingston", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Iris", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Babette", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Charlie Linen", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Sadie Ottoman", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Heidi Medley", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Gigi Lisa", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000)),
+    array("name"=>"Stacey Cafe", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, gold and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100.00, 30000.00), "qty"=>rand(1000, 10000)),
+    array("name"=>"Turner", "brand"=>rand(1,15),
+        "category"=>rand(1,6), "desc"=>"The classic leaf pattern in white, green and brown is old-fashioned and up-to-date at the same time. ",
+        "type"=>rand(1,22), "image"=>'', "cost"=>rand(100, 400), "qty"=>rand(1000, 10000))
 
+                );
 
+$testObj = new furniture();
+foreach($product_info as $value){
+        echo $value['name'];
+        $testObj->addFurniture($value['type'], $value['name'], $value['desc'], $value['category'],
+            $value['brand'], $value['image']);
+        $fid = $testObj->get_insert_id();
+        echo $fid;
+        $testObj->addInventoryDetails($fid, $value['qty'], $value['cost']);
 }
 
 
